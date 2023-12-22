e103d3870ae9b5db5c4ebbdc2bbd4e9835d99a54
Alex Dainiak <alex.dainiak@yandex.ru>
2023-12-22 22:47:58 +0400

    refactor

--- tcf-eo.js
+++ tcf-eo.js
@@ -1,19 +1,11 @@
 function getUniqueTags(objects) {
     const tagsSet = new Set();
-
-    objects.forEach(obj => {
-        if (obj.t && Array.isArray(obj.t)) {
-            obj.t.forEach(tag => tagsSet.add(tag));
-        }
-    });
-
+    objects.forEach(obj => obj.t && Array.isArray(obj.t) && obj.t.forEach(tag => tagsSet.add(tag)));
     return Array.from(tagsSet).sort();
 }
 
 function filterObjectsByTags(objects, tagsSubset) {
-    return objects.filter(obj => {
-        return tagsSubset.length === 0 || tagsSubset.every(tag => obj.t && obj.t.includes(tag));
-    });
+    return objects.filter(obj => tagsSubset.length === 0 || tagsSubset.every(tag => obj.t && obj.t.includes(tag)));
 }
 
 function createStopwatchButton(initialTime) {
@@ -31,7 +23,7 @@ function createStopwatchButton(initialTime) {
         }
         let minutes = Math.floor(timeLeft / 60);
         let seconds = timeLeft % 60;
-        span.innerHTML = "&nbsp;" + minutes + ":" + (seconds < 10 ? "0" + seconds : seconds);
+        span.innerHTML = `&nbsp;${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
     }
     function startTimer() {
         interval && clearInterval(interval);
@@ -102,14 +94,14 @@ function createToggles3(data) {
         ideasDiv = document.createElement('div');
         ideasDiv.id = 'ideasDiv';
         ideasDiv.classList.add('collapse');
-        const ideasList = document.createElement('ul');
+        const ideaList = document.createElement('ul');
         data.i.forEach(item => {
             const listItem = document.createElement('li');
             listItem.textContent = item;
-            ideasList.appendChild(listItem);
+            ideaList.appendChild(listItem);
         });
         ideasDiv.innerHTML = '<p><b>Voici quelques idées pour votre monologue :</b></p>';
-        ideasDiv.appendChild(ideasList);
+        ideasDiv.appendChild(ideaList);
     }
 
     if(data.e) {
@@ -145,20 +137,20 @@ function displayQuestion(questionObject) {
         const ul = document.createElement('ul');
         ul.className = 'card-text';
         const topic = document.createElement('li');
-        topic.innerHTML = '<b>Sujet du dialogue:</b> ' + questionObject.s + '. ';
+        topic.innerHTML = `<b>Sujet du dialogue:</b> ${questionObject.s}. `;
         ul.appendChild(topic);
         const evaluator = document.createElement('li');
-        evaluator.innerHTML = '<b>Rôle d’évaluateur:</b> ' + questionObject.ev + '. ';
+        evaluator.innerHTML = `<b>Rôle d’évaluateur:</b> ${questionObject.ev}. `;
         ul.appendChild(evaluator);
         const examinee = document.createElement('li');
-        examinee.innerHTML = '<b>Votre rôle:</b> ' + questionObject.ex + '. ';
+        examinee.innerHTML = `<b>Votre rôle:</b> ${questionObject.ex}. `;
         ul.appendChild(examinee);
         const goal = document.createElement('li');
-        goal.innerHTML = '<b>Votre objectif:</b> ' + questionObject.g + '. ';
+        goal.innerHTML = `<b>Votre objectif:</b> ${questionObject.g}. `;
         ul.appendChild(goal);
         const details = document.createElement('li');
         details.className = 'text-muted';
-        details.innerHTML = '<b>Détails possibles à demander:</b> ' + questionObject.d.join(', ') + '. ';
+        details.innerHTML = `<b>Détails possibles à demander:</b> ${questionObject.d.join(', ')}. `;
         ul.appendChild(details);
         divBody.appendChild(ul);
         divBody.appendChild(createToggles2(questionObject));
@@ -173,7 +165,7 @@ function displayQuestion(questionObject) {
     else {
         const p = document.createElement('p');
         p.className = 'card-text';
-        p.textContent = 'Pr\u00e9sentez-vous bri\u00e8vement et d\u00e9crivez ' + questionObject;
+        p.textContent = `Présentez-vous brièvement et décrivez ${questionObject}`;
         divBody.appendChild(p);
         divBody.appendChild(createStopwatchButton(120));
     }
@@ -193,7 +185,7 @@ function createCheckboxes(tags, prefix) {
         checkbox.checked = false;
         checkbox.className = 'form-check-input';
         checkbox.value = tag;
-        checkbox.id = prefix + 'checkbox' + index;
+        checkbox.id = `${prefix}checkbox${index}`;
 
         const label = document.createElement('label');
         label.className = 'form-check-label';
@@ -214,28 +206,25 @@ function gotoTranslation(e) {
     }
 
     let phrase = t.textContent.trim();
-    window.open('https://translate.google.com/?sl=fr&tl=en&text=' + phrase + '&op=translate', '_blank');
+    window.open(`https://translate.google.com/?sl=fr&tl=en&text=${phrase}&op=translate`, '_blank');
 }
 
 for(let taskNumber of [1, 2, 3]) {
-    const questions = window['part' + taskNumber + '_questions'];
+    const questions = window[`part${taskNumber}_questions`];
     const tags = getUniqueTags(questions);
-    const checkboxes = createCheckboxes(tags, 'task' + taskNumber + '_');
-    const tagsContainer = document.getElementById('tags' + taskNumber);
-    checkboxes.forEach(checkbox => {
-        tagsContainer.appendChild(checkbox);
-    });
+    const checkboxes = createCheckboxes(tags, `task${taskNumber}_`);
+    const tagsContainer = document.getElementById(`tags${taskNumber}`);
+    checkboxes.forEach(checkbox => tagsContainer.appendChild(checkbox));
 
-
-    document.getElementById('random' + taskNumber).addEventListener('click', () => {
+    document.getElementById(`random${taskNumber}`).addEventListener('click', () => {
         const checkedTags = taskNumber > 1 && Array.from(tagsContainer.querySelectorAll('input[type="checkbox"]:checked')).map(checkbox => checkbox.value);
         const objects = taskNumber > 1 ? filterObjectsByTags(questions, checkedTags) : questions;
         const randomObject = objects[Math.floor(Math.random() * objects.length)];
-        document.getElementById('content' + taskNumber).innerHTML = '';
-        document.getElementById('content' + taskNumber).appendChild(displayQuestion(randomObject));
+        document.getElementById(`content${taskNumber}`).innerHTML = '';
+        document.getElementById(`content${taskNumber}`).appendChild(displayQuestion(randomObject));
     });
 
-    const contentContainer = document.getElementById('content' + taskNumber);
+    const contentContainer = document.getElementById(`content${taskNumber}`);
     contentContainer.innerHTML = '← Cliquez sur le bouton à gauche pour sélectionner une question aléatoire.';
     contentContainer.addEventListener('dblclick', gotoTranslation);
 }
